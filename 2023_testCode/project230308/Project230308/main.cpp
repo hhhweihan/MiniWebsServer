bf6f8ea490a01f8fad32867e307d129e6ec68aa0
WH1024 <1258222155@qq.com>
2023-03-08 09:21:39 -0800

    recv step

--- 2023_testCode/project230308/Project230308/main.cpp
+++ 2023_testCode/project230308/Project230308/main.cpp
@@ -4,6 +4,8 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<unistd.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
 #define closesocket close
 #endif
 
@@ -22,7 +24,33 @@ int main(int argc, char* argv[])
         printf("create socket failed!\n");
     }
     printf("[%d]", sock);
-   close(sock);
-    getchar();
+    unsigned short port = 8080;
+    if(argc > 1)
+    {
+        port = atoi(argv[1]);
+    }
+    sockaddr_in saddr;
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
+    //close(sock);
+    //getchar();
+    saddr.sin_addr.s_addr = htonl(0);
+    if(bind(sock, (sockaddr*)&saddr, sizeof(saddr)) != 0)
+    {
+        printf("bind port %d failed!\n", port);
+    }
+    printf("bind port %d success!\n", port); 
+    listen(sock, 10);
+   
+    sockaddr_in caddr;
+    socklen_t len = sizeof(caddr);
+
+    int client = accept(sock,(sockaddr*)&caddr, &len);
+    printf("accept client %d\n", client);
+    char * ip = inet_ntoa(caddr.sin_addr);
+    unsigned short cport = ntohs(caddr.sin_port);
+    printf("client ip is %s, port is %d\n", ip, cport);
+
+    closesocket(client);
     return 0;
 }
