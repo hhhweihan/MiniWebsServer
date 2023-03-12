132dc405027525c86d8c3a8cf30639f84fe8cbcc
WH1024 <1258222155@qq.com>
2023-03-12 08:05:52 -0700

    Package class

--- 2023_testCode/project230308/Project230308/main.cpp
+++ 2023_testCode/project230308/Project230308/main.cpp
@@ -1,19 +1,8 @@
-#include<string.h>
 #include<stdlib.h>
-#include<stdio.h>
-
-#ifdef WIN32
-#include<windows.h>
-#else 
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#define closesocket close
-#endif
-
+#include"XTcp.h"
 #include<thread>
-using namespace std;
+#include<string.h>
+
 class TcpThread
 {
 public:
@@ -22,71 +11,44 @@ public:
         char buf[1024] = {0};
         for(;;)
         {
-            int recvlen = recv(client, buf, sizeof(buf)-1, 0);
+            int recvlen = client.Recv(buf, sizeof(buf)-1);
             if(recvlen <= 0) break;
             buf[recvlen] = '\0';
             if(strstr(buf, "quit") != NULL)
             {
                 char re[] = "quit success!\n";
-                send(client, re, strlen(re)+1, 0);
+                client.Send(re, strlen(re)+1);
                 break;
             }
-            int sendlen = send(client, "ok\n", 4, 0);
+            int sendlen = client.Send("ok\n", 4);
             printf("recv %s\n", buf);
         }
-        closesocket(client);
+        client.Close();
         delete this;
     }
-    int client = 0;
+    XTcp client;
 };
+
 
 int main(int argc, char* argv[])
 {
-#ifdef WIN32
-    WSADATA ws;
-    WSAStartup(MAKEWORD(2, 2), &ws);
-#endif 
-
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-    {
-        printf("create socket failed!\n");
-    }
-    printf("[%d]", sock);
     unsigned short port = 8080;
-    if(argc > 1)
+    if (argc > 1)
     {
         port = atoi(argv[1]);
     }
-    sockaddr_in saddr;
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(port);
-    //close(sock);
-    //getchar();
-    saddr.sin_addr.s_addr = htonl(0);
-    if(bind(sock, (sockaddr*)&saddr, sizeof(saddr)) != 0)
-    {
-        printf("bind port %d failed!\n", port);
-    }
-    printf("bind port %d success!\n", port); 
-    listen(sock, 10);
-   
+    XTcp server;
+    server.CreateSocket();
+    server.Bind(port);
     for(;;)
     {
-        sockaddr_in caddr;
-        socklen_t len = sizeof(caddr);
-        int client = accept(sock,(sockaddr*)&caddr, &len);
-        if(client <= 0) break;
-        printf("accept client %d\n", client);
-        char * ip = inet_ntoa(caddr.sin_addr);
-        unsigned short cport = ntohs(caddr.sin_port);
-        printf("client ip is %s, port is %d\n", ip, cport);
+        XTcp client = server.Accept();
         TcpThread *th = new TcpThread();
         th->client = client;
-        thread sth(&TcpThread::Main, th);
+        std::thread sth(&TcpThread::Main, th);
         sth.detach(); // shifang ziyuan
     }
-   closesocket(sock);
+    server.Close();
     getchar();
     return 0;
 }
